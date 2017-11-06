6bbf1dac5ffd8ba369436d1d69bba6e25c2a706a
Vicent Santamarta <vicent.santamarta@gmail.com>
2017-11-06 23:22:20 +0100

    Ejercicios

---
+++ Práctica 2/mueveLaCamara - Original.js
@@ -0,0 +1,263 @@
+
+var gl, program;
+var myTorus;
+var myphi = 0, zeta = 30, radius = 15, fovy = Math.PI/10;
+var selectedPrimitive = exampleCone;
+
+function getWebGLContext() {
+    
+  var canvas = document.getElementById("myCanvas");
+    
+  var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+    
+  for (var i = 0; i < names.length; ++i) {
+    try {
+      return canvas.getContext(names[i]);
+    }
+    catch(e) {
+    }
+  }
+    
+  return null;
+
+}
+
+function initShaders() {
+    
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, document.getElementById("myVertexShader").text);
+  gl.compileShader(vertexShader);
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(vertexShader));
+    return null;
+  }
+ 
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, document.getElementById("myFragmentShader").text);
+  gl.compileShader(fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    alert(gl.getShaderInfoLog(fragmentShader));
+    return null;
+  }
+  
+  program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  
+  gl.linkProgram(program);
+    
+  gl.useProgram(program);
+    
+  program.vertexPositionAttribute = gl.getAttribLocation(program, "VertexPosition");
+  gl.enableVertexAttribArray(program.vertexPositionAttribute);
+
+  program.modelViewMatrixIndex  = gl.getUniformLocation(program,"modelViewMatrix");
+  program.projectionMatrixIndex = gl.getUniformLocation(program,"projectionMatrix");
+
+}
+
+function initRendering() {
+
+  gl.clearColor(0.0,0.15,0.15,1.0);
+  gl.lineWidth(1.5);
+
+}
+
+function initBuffers(model) {
+    
+  model.idBufferVertices = gl.createBuffer ();
+  gl.bindBuffer (gl.ARRAY_BUFFER, model.idBufferVertices);
+  gl.bufferData (gl.ARRAY_BUFFER, new Float32Array(model.vertices), gl.STATIC_DRAW);
+    
+  model.idBufferIndices = gl.createBuffer ();
+  gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, model.idBufferIndices);
+  gl.bufferData (gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(model.indices), gl.STATIC_DRAW);
+
+}
+
+function drawWire(model) {
+    
+  gl.bindBuffer (gl.ARRAY_BUFFER, model.idBufferVertices);
+  gl.vertexAttribPointer (program.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+    
+  gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, model.idBufferIndices);
+  for (var i = 0; i < model.indices.length; i += 3)
+    gl.drawElements (gl.LINE_LOOP, 3, gl.UNSIGNED_SHORT, i*2);
+
+}
+
+function initPrimitives() {
+
+  initBuffers(examplePlane);
+  initBuffers(exampleCube);
+  initBuffers(exampleCone);
+  initBuffers(exampleCylinder);
+  initBuffers(exampleSphere);
+
+  myTorus = makeTorus(0.5, 1, 10, 20);
+  initBuffers(myTorus);
+  
+}
+
+function setProjection() {
+    
+  var projectionMatrix  = mat4.create();
+  mat4.perspective(projectionMatrix, fovy, 1.0, 0.1, 100.0);
+  
+  gl.uniformMatrix4fv(program.projectionMatrixIndex,false,projectionMatrix);
+
+}
+
+function getCameraMatrix() {
+    
+  var _phi  = myphi * Math.PI / 180.0;
+  var _zeta = zeta  * Math.PI / 180.0;
+    
+  // conversion de coordenadas polares (_zeta,_phi) a rectangulares (x, y, z)
+  // ver: https://en.wikipedia.org/wiki/Spherical_coordinate_system
+  var x = radius * Math.cos(_zeta) * Math.sin(_phi);
+  var y = radius * Math.sin(_zeta);
+  var z = radius * Math.cos(_zeta) * Math.cos(_phi);
+
+  var cameraMatrix = mat4.create();
+  mat4.lookAt(cameraMatrix, [x, y, z], [0, 0, 0], [0, 1, 0]);
+    
+  return cameraMatrix;
+
+}
+
+function drawScene() {
+    
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  setProjection();
+
+  // 1. calcula la matriz de transformación del modelo-vista
+  var modelMatrix     = mat4.create();
+  var modelViewMatrix = mat4.create();
+  mat4.fromScaling(modelMatrix, [0.5, 0.5, 0.5]);
+  mat4.multiply(modelViewMatrix, getCameraMatrix(), modelMatrix);
+    
+  // 2. envía la matriz calculada al shader de vértices
+  gl.uniformMatrix4fv(program.modelViewMatrixIndex, false, modelViewMatrix);
+    
+  // 3. dibuja la primitiva actualmente seleccionada
+  drawWire(selectedPrimitive);
+
+}
+
+function initHandlers() {
+    
+  var mouseDown = false;
+  var lastMouseX;
+  var lastMouseY;
+
+  var canvas     = document.getElementById("myCanvas");
+  var htmlPhi    = document.getElementById("Phi");
+  var htmlZeta   = document.getElementById("Zeta");
+  var htmlRadius = document.getElementById("Radius");
+  var htmlFovy   = document.getElementById("Fovy");
+
+  htmlPhi.innerHTML    = myphi.toFixed(0);
+  htmlZeta.innerHTML   = zeta;
+  htmlRadius.innerHTML = radius.toFixed(1);
+  htmlFovy.innerHTML   = fovy.toFixed(2);
+
+  canvas.addEventListener("mousedown",
+    function(event) {
+      mouseDown  = true;
+      lastMouseX = event.clientX;
+      lastMouseY = event.clientY;
+    },
+    false);
+
+  canvas.addEventListener("mouseup",
+    function() {
+      mouseDown = false;
+    },
+    false);
+
+  canvas.addEventListener("mousemove",
+    function (event) {
+      if (!mouseDown) {
+        return;
+      }
+      var newX = event.clientX;
+      var newY = event.clientY;
+      if (event.shiftKey == 1) {
+        if (event.altKey == 1) {              
+          // fovy
+          fovy -= (newY - lastMouseY) / 100.0;
+          if (fovy < 0.001) {
+            fovy = 0.1;
+          }
+          //console.log("fovy = " + fovy);
+          htmlFovy.innerHTML = fovy.toFixed(2);
+        } else {                              
+          // radius
+          radius -= (newY - lastMouseY) / 10.0;
+          if (radius < 0.01) {
+            radius = 0.01;
+          }
+          //console.log("radius = " + radius);
+          htmlRadius.innerHTML = radius.toFixed(1);
+        }
+      } else {                                 // position
+        myphi  -= (newX - lastMouseX);
+        zeta += (newY - lastMouseY);
+        if (zeta < -80) {
+          zeta = -80.0;
+        }
+        if (zeta > 80) {
+          zeta = 80;
+        }
+        //console.log("phi = " + myphi + ", zeta = " + zeta);
+        //console.log("phi = " + myphi + ", zeta = " + zeta);
+        htmlPhi.innerHTML = myphi.toFixed(0);
+        htmlZeta.innerHTML = zeta;
+      }
+      lastMouseX = newX
+      lastMouseY = newY;
+      requestAnimationFrame(drawScene);
+    },
+    false);
+
+  var botones = document.getElementsByTagName("button");
+  
+  for (var i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("click",
+    function(){
+      switch (this.innerHTML) {
+        case "Plano":    selectedPrimitive = examplePlane;    break;
+        case "Cubo":     selectedPrimitive = exampleCube;     break;
+        case "Cono":     selectedPrimitive = exampleCone;     break;
+        case "Cilindro": selectedPrimitive = exampleCylinder; break;
+        case "Esfera":   selectedPrimitive = exampleSphere;   break;
+        case "Toro":     selectedPrimitive = myTorus;         break;
+      }
+      requestAnimationFrame(drawScene);
+    },
+    false);
+  }
+  
+}        
+
+function initWebGL() {
+    
+  gl = getWebGLContext();
+    
+  if (!gl) {
+    alert("WebGL no está disponible");
+    return;
+  }
+    
+  initShaders();
+  initPrimitives();
+  initRendering();
+  initHandlers();
+  
+  requestAnimationFrame(drawScene);
+  
+}
+
+initWebGL();
